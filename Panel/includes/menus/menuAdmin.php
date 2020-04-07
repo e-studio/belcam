c36978af5ce7ad41d0daf097f61a5a79e291f0eb
RayadoxAV <rayadoxav@hotmail.com>
2020-04-07 17:14:37 -0600

    Cambios
    Actualización de fontawesome y pantallas de remolques

--- Panel/includes/menus/menuAdmin.php
+++ Panel/includes/menus/menuAdmin.php
@@ -70,6 +70,7 @@
             <li><a href="regUsuario.php"><i class="fa fa-user-plus"></i> Usuario</a></li>
             <li><a href="regProducto.php"><i class="fa fa-user-plus"></i> Producto</a></li>
             <li><a href="regUnidad.php"><i class="fa fa-truck"></i> Unidad</a></li>
+            <li><a href="regRemolque.php"><i class="fas fa-trailer"></i> Remolque</a></li>
             <li>-</li>
 
             <li><a href="listaChoferes.php"><i class="fa fa-group"></i> Choferes</a></li>
@@ -78,6 +79,7 @@
             <li><a href="listaUsuarios.php"><i class="fa fa-group"></i> Usuarios</a></li>
             <li><a href="listaProductos.php"><i class="fa fa-group"></i> Productos</a></li>
             <li><a href="listaUnidades.php"><i class="fa fa-truck"></i> Unidades</a></li>
+            <li><a href="listaRemolques.php"><i class="fas fa-trailer"></i> Remolques</a></li>
           </ul>
         </li>
 
